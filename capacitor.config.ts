9ddfa7d0e0a8b47bc355a3665606a3ae4a685e7b
marcos mora <abreu760@hotmail.com>
2026-02-03 23:26:27 -0400

    updates

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ecme.app',
+  appName: 'ecme',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
--- vite.config.ts
+++ vite.config.ts
@@ -24,6 +24,6 @@ export default defineConfig({
     }
   },
   build: {
-    outDir: 'build'
+    outDir: 'dist'
   }
 })
