c6abca743a1c113e8261d4cd60ca28ea966b752d
Marcos Mora <abreu760@hotmail.com>
2026-02-15 23:45:22 +0000

    feat: agrega soporte para cron jobs y mejora en la configuración de extensiones

---
+++ api/cron.js
@@ -0,0 +1,6 @@
+export default function handler(req, res) {
+    if (req.headers.get('Authorization') !== `Bearer ${process.env.CRON_SECRET}`) {
+        return res.status(401).end('Unauthorized');
+    }
+    res.status(200).end('Hello Cron!');
+}
